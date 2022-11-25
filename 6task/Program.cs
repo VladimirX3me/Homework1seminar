54ad8d8436a1f88200504caacbfc731c3b42de4d
Vladimir <vladxtreame@gmail.com>
2022-11-25 22:41:50 +0400

    add 6task

---
+++ 6task/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вод принимает число и выдаёт, является ли число чётным (Делится ли оно на 2 без остатка).
+
+ Console.WriteLine("Enter number");
+ 
+ int a = Convert.ToInt32(Console.Read());
+                
+if (a % 2 == 0) 
+    Console.WriteLine("Четное");
+    else Console.WriteLine("Нечетное");
+    Console.ReadKey();
