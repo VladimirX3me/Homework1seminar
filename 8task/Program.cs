ae6f130a97c1589e2aad087b9e67a769ac990747
Vladimir <vladxtreame@gmail.com>
2022-11-25 22:53:31 +0400

    add 8task

---
+++ 8task/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Enter number ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+byte num = 1;
+            while (num <= num1)
+            {
+                int result = num % 2;
+                if (result != 1)
+                {
+                    Console.WriteLine("четное " + num);
+                    num++;
+                }
+                else
+                {
+                    num++;
+                }
+            }
