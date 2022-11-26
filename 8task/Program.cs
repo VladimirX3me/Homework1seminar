f2f42cd05582672125aafbb8bbe31e93fb7e897f
VladimirX3me <118298364+VladimirX3me@users.noreply.github.com>
2022-11-26 12:53:30 +0400

    Update Program.cs

--- 8task/Program.cs
+++ 8task/Program.cs
@@ -1,4 +1,5 @@
-﻿
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает его чётные числа от 1 до N.
+
 Console.Write("Enter number ");
 
 int num1 = Convert.ToInt32(Console.ReadLine());
